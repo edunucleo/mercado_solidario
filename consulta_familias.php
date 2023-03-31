abb97419102f87ea5b981face161bd1f3d5ae4f5
Eduardo Vieira de Campos <edunucleo@gmail.com>
2023-03-31 11:16:10 -0300

    formata

--- consulta_familias.php
+++ consulta_familias.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!-- jquery -->
     <script defer src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
@@ -146,82 +146,76 @@
                         </div>
                         <div class="modal-body">
 
-                            <table class="table table-striped table-bordered" >
+                            <table class="table table-striped table-bordered">
                                 <tbody>
-                                    <tr >
+                                    <tr>
                                         <td><strong>Nome Completo</strong></td>
                                     </tr>
-                                    <tr >
-
+                                    <tr>
                                         <td>edu4</td>
                                     </tr>
                                     <tr>
-                                        <td ><strong>E-mail</strong></td>
+                                        <td><strong>E-mail</strong></td>
                                     </tr>
-                                    <tr >
-                                        
+                                    <tr>
                                         <td>edunucleo@gmail.com</td>
                                     </tr>
-                                    <tr >
+                                    <tr>
                                         <td colspan="2"><strong>Endereço</strong></td>
                                     </tr>
-                                    <tr >
-                                       
+                                    <tr>
                                         <td>Rua Sto Antonio, 775 - cento - tatuí</td>
                                     </tr>
-                                    <tr >
-                                        <td ><strong>Sua família participa de algum Programa Social de auxílio do governo? Qual?</strong></td>
+                                    <tr>
+                                        <td><strong>Sua família participa de algum Programa Social de auxílio do governo? Qual?</strong></td>
                                     </tr>
-                                    <tr >
-                                        
+                                    <tr>
                                         <td>Bolsa família</td>
                                     </tr>
-                                    <tr >
-                                        <td ><strong>Data de Nascimento</strong></td>
+                                    <tr>
+                                        <td><strong>Data de Nascimento</strong></td>
                                     </tr>
-                                    <tr >
-                                       
+                                    <tr>
                                         <td>02-03-2000</td>
                                     </tr>
-                                    <tr >
-                                        <td ><strong>Celular</strong></td>
+                                    <tr>
+                                        <td><strong>Celular</strong></td>
                                     </tr>
-                                    <tr >
-                                        
+                                    <tr>
                                         <td>15996008878</td>
                                     </tr>
                                     <tr>
-                                        <td ><strong>Qual é a renda financeira mensal da família?</strong></td>
+                                        <td><strong>Qual é a renda financeira mensal da família?</strong></td>
                                     </tr>
                                     <tr>
-                                        
+
                                         <td>&lt;1</td>
                                     </tr>
                                     <tr>
                                         <td><strong>Quantas pessoas moram com você? Coloque o nome e idade: (inclusive crianças e bebês)</strong></td>
                                     </tr>
-                                    <tr >
-                                        
+                                    <tr>
+
                                         <td>10</td>
                                     </tr>
-                                    <tr >
-                                        <td ><strong>Ao preencher este formulário você concorda com os termos e política do Projeto Casa do Pão, e autoriza a divulgação da sua imagem para depoimentos e registros do Projeto?</strong></td>
+                                    <tr>
+                                        <td><strong>Ao preencher este formulário você concorda com os termos e política do Projeto Casa do Pão, e autoriza a divulgação da sua imagem para depoimentos e registros do Projeto?</strong></td>
                                     </tr>
-                                    <tr >
-                                        
+                                    <tr>
+
                                         <td>sim</td>
                                     </tr>
                                     <tr class="">
-                                        <td ><strong>Pontos</strong></td>
+                                        <td><strong>Pontos</strong></td>
                                     </tr>
-                                    <tr >
-                                        
+                                    <tr>
+
                                         <td>0</td>
                                     </tr>
-                                    <tr >
-                                        <td ><strong>Aprovado</strong></td>
+                                    <tr>
+                                        <td><strong>Aprovado</strong></td>
                                     </tr>
-                                    <tr >
+                                    <tr>
                                         <td>0</td>
                                     </tr>
                                 </tbody>
