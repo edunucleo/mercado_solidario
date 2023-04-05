2cc9b8ffcc3a184a9fefd66e5652a882ca0a1f56
Eduardo Vieira de Campos <edunucleo@gmail.com>
2023-04-05 09:42:40 -0300

    inicio correção de dao produto e marca

--- consulta_produtos.php
+++ consulta_produtos.php
@@ -58,37 +58,7 @@ $produtoDao->delete(1);
 <body>
     <div class="d-flex flex-nowrap ">
 
-        <!--Sidebar-->
-
-        <div class="d-flex flex-column flex-shrink-0  p-3 text-bg-dark" style="width: 280px; height: 100vh;">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-
-                <span class="fs-4"><i class="bi bi-bag-heart-fill"></i> Administrativo</span>
-            </a>
-            <hr>
-            <ul class="nav nav-pills flex-column mb-auto">
-                <li class="nav-item">
-                    <a href="consulta_familias.php" class="nav-link" aria-current="page">
-                        <i class="bi bi-people-fill"></i>
-                        Lista de Famílias
-                    </a>
-                </li>
-                <li>
-                    <a href="consulta_produtos.php" class="nav-link text-white active">
-                        <i class="bi bi-basket-fill"></i>
-                        Lista de Produtos/Estoque
-                    </a>
-                </li>
-            </ul>
-            <hr>
-            <div class="dropdown">
-                <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://avatars.githubusercontent.com/u/6980543?s=40&v=4" alt="" width="32" height="32" class="rounded-circle me-2">
-                    <strong>Eduardo</strong>
-                </a>
-            </div>
-        </div>
-
-        <!--fim da div sidebar-->
+ <?php include_once"sidebar.php";?>
+ 
 </body>
 </html>
