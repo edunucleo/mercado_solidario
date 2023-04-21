6c9722b4e100d9713b65969094e118985635a140
Eduardo Vieira de Campos <edunucleo@gmail.com>
2023-04-21 12:20:51 -0300

    continuação dos controllers

--- js/script.js
+++ js/script.js
@@ -8,10 +8,27 @@ $(document).ready(function () {
 
 });
 
-function carregarCadastro(id) {
-  $("#carrega_cadastro").load(`grava_mostra_edita_familia.php`,
-   { idcadastro: id }
+function carregar(id,formulario) {
+  $("#carrega_cadastro").load(`edita_familia.php`,
+   { id: id ,
+    formulario: formulario}
     );
 }
+
+function excluir(id,formulario) {
+  $("#carrega_cadastro").load(`grava_mostra_edita_familia.php`,
+   { id: id,
+    formulario: formulario },
+    
+    );
+}
+function atualizarPontos(id,pontos, formulario) {
+  $("#carrega_cadastro").load(`grava_mostra_edita_familia.php`,
+   { id: id,
+    pontos: pontos,
+    formulario: formulario }
+    );
+}
+
 
 
